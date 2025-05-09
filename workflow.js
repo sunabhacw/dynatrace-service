75f68e31ac5f1c84ca48122e2cbf976bdf305b7d
sunabhacw <sunabha.sinha@connectwise.com>
2025-05-09 13:41:17 +0530

    added fix

--- workflow.js
+++ workflow.js
@@ -119,6 +119,36 @@ export default async function ({ executionId }) {
       }
     }
 
+    const fetch = require("node-fetch"); // Ensure you have node-fetch installed
+
+    const url =
+      "https://podcatalogueui.int.use1.cwcoreentitydev.cwnet.io/api/webhook/component/list";
+    const body = {
+      statusPageId: "61e1ea9b682d750536ef4cdb",
+    };
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        token:
+          "eyJhbGciOiJSUzI1NiIsImtpZCI6IkVBNjUxNTREMDI5QzkwNEFEOENGODk1RTkwQTNENjE3NzlDMzkzNTJSUzI1NiIsIng1dCI6IjZtVVZUUUtja0VyWXo0bGVrS1BXRjNuRGsxSSIsInR5cCI6ImF0K2p3dCJ9.eyJpc3MiOiJodHRwczovL3N0cy5jb25uZWN0d2lzZWRldi5jb20iLCJuYmYiOjE3NDY3MDMxNzcsImlhdCI6MTc0NjcwMzE3NywiZXhwIjoxNzQ2NzA2Nzc3LCJzY29wZSI6WyJvcGVuaWQiXSwiYW1yIjpbInB3ZCJdLCJjbGllbnRfaWQiOiJwb2QtY2F0YWxvZ3VlLWNsaWVudCIsInN1YiI6IjY2NGYyNzM5YTA3NTBjYTdhNDQwYmNiZiIsImF1dGhfdGltZSI6MTc0NjcwMzE3NSwiaWRwIjoibG9jYWwiLCJwYXJ0bmVyX2lkIjoiNjQ3ZDc3NjMyNTA0NDYwMDAxYzNlYjI4Iiwic2lkIjoiOUQ0MDk3NjFFNTE0MzY4RkY0ODIxRENGQkU1RTcyMTYifQ.MrmD7ZgT0sDdGe7HiIdyQEp_jppOITzKI2VZKpOt44iN3_ZzVj7n99bgzbOc6g3Z1gLLyN5QWFfDuYpM6ZeNxSWkWAr61YI3rdIy6gtrGZFmjaDfnJfNVRSjxUe-VzpkVhP7fH5UYEdD_iJ3bBfJjH_Hohnwfhm4rki40DVko7ETPkIyp11Q2qJyrfB1XXT2qObQnfIN7AtfvsRbUJlRuW6eM9UqTL_QtAQxRngIGDmGB2ii5TWEya2TqnynRgtTJSIoEXxNr2dgXDJlBMxZpEuxJHOXoPgP710mXdN18w_l_XsvTT23I8Sj5DJxTD0OoMTOQRkUYEYduH-iMIXQTA",
+      },
+      body: JSON.stringify(body),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Response data:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error.message);
+      });
+
     console.log("Final extracted details:", JSON.stringify(result, null, 2));
     return result;
   } catch (error) {
